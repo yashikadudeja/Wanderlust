1ce5b11a074f85c0c809af1482e591a029224dd4
Yashikadudeja <yashikadudeja486@gmail.com>
2025-05-17 13:07:15 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV != "production"){
+if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
 
@@ -16,12 +16,12 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-const listingRouter= require("./routes/listing.js");
+const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const dbUrl = process.env.ATLASDB_URL;
+// use the same name as in your .env
+const dbUrl = process.env.MONGO_URL;            // was process.env.ATLASDB_URL
 
 main()
     .then(() => {
@@ -35,40 +35,36 @@ async function main() {
     await mongoose.connect(dbUrl);
 }
 
-app.set("view engine" , "ejs");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs" , ejsMate);
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
-    mongoUrl : dbUrl,
-    crypto : {
-        secret : process.env.SECRET,
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SESSION_SECRET,   // was process.env.SECRET
     },
-    touchAfter : 24 * 3600,
+    touchAfter: 24 * 3600,
 });
 
-store.on("error" , () => {
-    console.log("ERROR IN MONGO SESSION STORE" , err); 
+store.on("error", (err) => {                 // added `err` param
+    console.log("ERROR IN MONGO SESSION STORE", err);
 });
 
 const sessionOptions = {
     store,
-    secret :  process.env.SECRET,
-    resave : false,
-    saveUninitialized : true,
-    cookie : {
-        expires : Date.now() + 7 * 24 * 60 * 60 * 1000, 
-        maxAge : 7 * 24 * 60 * 60 * 1000, 
-        httpOnly : true,
+    secret: process.env.SESSION_SECRET,      // was process.env.SECRET
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
     },
 };
-
-// app.get("/" , (req,res) => {
-//     res.send("Hi , I am root");
-// });
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -81,36 +77,25 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    console.log("Current User:", req.user); 
+    console.log("Current User:", req.user);
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
     next();
 });
 
-// app.get("/demouser" , async (req, res) => {
-//     let fakeUser = new User({
-//         email : "student@gmail.com",
-//         username : "delta-student"
-//     });
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews/", reviewRouter);
+app.use("/", userRouter);
 
-//     let registeredUser = await User.register(fakeUser , "helloworld");
-//     res.send(registeredUser);
-// });
-
-app.use("/listings" , listingRouter);
-app.use("/listings/:id/reviews/" , reviewRouter);
-app.use("/" , userRouter);
-
-app.all("*" , (req, res, next) => {
+app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
 });
 
-app.use((err,req,res,next) => {
-    let {statusCode = 500 , message = "Something Went Wrong!" } = err;
-    res.status(statusCode).render("error.ejs" , { message });
-    // res.status(statusCode).send(message);
-})
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = "Something Went Wrong!" } = err;
+    res.status(statusCode).render("error.ejs", { message });
+});
 
 app.listen(1337, () => {
     console.log("server is listening on the port 1337");
--- controllers/listings.js
+++ controllers/listings.js
@@ -1,6 +1,6 @@
 const Listing = require("../models/listing");
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapToken = process.env.MAP_TOKEN;
+const mapToken = process.env.MAPBOX_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req,res) => {
